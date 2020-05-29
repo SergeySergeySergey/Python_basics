796209f9dbd016fcf170aa23a1c4981c18bf919a
Sergey <callmedjin@mail.ru>
2020-05-29 15:10:00 +0300

    added task 1,4

---
+++ task_4.py
@@ -0,0 +1,36 @@
+n=int(input("Введите целое число: "))
+while n>0:
+    one=n%10
+    n//=10
+    a=one
+    if a==one:
+        print(a)
+        break
+while n > 0:
+    two=n % 10
+    n //= 10
+    x=two
+    if x <= two:
+        print(x)
+        break
+while n > 0:
+    three=n % 10
+    n //= 10
+    b=7
+    if 7 <= three:
+        print(b)
+        break
+while n > 0:
+    four=n % 10
+    n //= 10
+    c=6
+    if 6 <= four:
+        print(c)
+        break
+while n > 0:
+    five=n % 10
+    n //= 10
+    d=5
+    if 5 <= five:
+        print(d)
+        break
