eedb48a92e16d57e73ef15e3580de2905497347f
Sergey <callmedjin@mail.ru>
2020-06-02 15:04:11 +0300

    fixed lesson 1 task 4

--- task_4.py
+++ task_4.py
@@ -1,36 +1,10 @@
-n=int(input("Введите целое число: "))
-while n>0:
-    one=n%10
-    n//=10
-    a=one
-    if a==one:
-        print(a)
-        break
-while n > 0:
-    two=n % 10
-    n //= 10
-    x=two
-    if x <= two:
-        print(x)
-        break
-while n > 0:
-    three=n % 10
-    n //= 10
-    b=7
-    if 7 <= three:
-        print(b)
-        break
-while n > 0:
-    four=n % 10
-    n //= 10
-    c=6
-    if 6 <= four:
-        print(c)
-        break
-while n > 0:
-    five=n % 10
-    n //= 10
-    d=5
-    if 5 <= five:
-        print(d)
-        break
+num=int(input("Введите целое положительное число: "))
+biggest_num = 0
+while num > 0:
+    last_num = num % 10
+    if last_num > biggest_num:
+        biggest_num = last_num
+        if biggest_num == 9:
+            break
+    num= num //10
+print(f"Наибольшая цифра в числе {biggest_num}")
