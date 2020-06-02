75a48de8b3b812131245257e4ac18faa9d7620cf
Sergey <callmedjin@mail.ru>
2020-06-02 15:16:50 +0300

    fixed rounding lesson1 task3

--- task_5.py
+++ task_5.py
@@ -1,14 +1,14 @@
-revenue=int(input("Введите сумму выручки: "))
-expenses=int(input("Введите сумму издержек: "))
+revenue=float(input("Введите сумму выручки: "))
+expenses=float(input("Введите сумму издержек: "))
 profit=revenue-expenses
 loss=expenses-revenue
 if profit>loss:
-    print("Сумма прибыли составляет: ", profit )
+    print(f"Сумма прибыли составляет: {profit:.3f}" )
 elif loss>profit:
-    print("Сумма убытка составляет: ", loss )
+    print(f"Сумма убытка составляет: {loss:.3f}" )
 if profit>loss:
     roa=revenue/profit
-    print("Рентабельность выручки: ", roa)
+    print(f"Рентабельность выручки: {roa:.3f}")
     man=int(input("Введите численность сотрудников: "))
     man_profit= profit/man
-    print("Прибыль на одного сотрудника составляет: ", man_profit)
+    print(f"Прибыль на одного сотрудника составляет: {man_profit:.3f}")
