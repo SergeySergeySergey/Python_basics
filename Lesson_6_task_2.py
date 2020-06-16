e664836f7beed700d3d5f8c9301827a6037f46ee
Sergey <callmedjin@mail.ru>
2020-06-16 15:02:47 +0300

    Fixed Lesson_6_task_2
    Исправил ошибки в задании 2 согласно вашему сообщению

--- Lesson_6_task_2.py
+++ Lesson_6_task_2.py
@@ -1,13 +1,17 @@
 class Road:
-    try:
-        _length=int(input("Введите длинну дорожного полотона в метрах: "))
-    except ValueError:
-        print("Допустим ввод только цифр!")
-    try:
-        _width=int(input("Введите ширину дорожного полотна в метрах: "))
-    except ValueError:
-        print("Допустим ввод только цифр!")
+    _length = input("Введите длинну дорожного полотона в метрах: ")
+    _width = input("Введите ширину дорожного полотна в метрах: ")
     def __init__(self):
+        try:
+            Road._length = int(Road._length)
+        except ValueError:
+            print("Допустим ввод только цифр!")
+            exit(0)
+        try:
+            Road._width = int(Road._width)
+        except ValueError:
+            print("Допустим ввод только цифр!")
+            exit(0)
         formula=(Road._length*Road._width*25*5)/1000
         print(f"Расчетная масса асфальта: {formula} тонн")
 r=Road()
